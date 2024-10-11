3ec2aee785ba2cd9f84b5c21272a9fe3191a4f5f
Lasse H. Bomholt <lasse@bomh.net>
2024-10-11 20:33:45 +0200

    cleanup

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { createQRCode } from "./qrcode.js";
+import "./jsQR.js";
 
 function getRandomChars(length) {
   return String.fromCharCode.apply(
@@ -7,32 +8,82 @@ function getRandomChars(length) {
   );
 }
 
+async function getImageData(url) {
+  return await new Promise((res) => {
+    const image = new Image();
+    image.src = url;
+
+    image.onload = () => {
+      const { naturalWidth: width, naturalHeight: height } = image;
+
+      const canvas = document.createElement("canvas");
+      canvas.height = height + 2;
+      canvas.width = width + 2;
+
+      const ctx = canvas.getContext("2d");
+      ctx.imageSmoothingEnabled = false;
+
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(image, 1, 1);
+
+      const scale = 1.5;
+
+      ctx.scale(scale, scale);
+
+      const imageData = ctx.getImageData(
+        0,
+        0,
+        (width + 2) * scale,
+        (height + 2) * scale
+      );
+
+      res([imageData, canvas.toDataURL()]);
+    };
+  });
+}
+
 const img = document.getElementById("qrcode");
 img.style.imageRendering = "pixelated";
 
-window.start = () => {
-  console.clear();
-  const tests = 150;
+(async () => {
+  const n = 100;
+  const max = 2331;
+  let misses = 0;
+  let fails = 0;
+  for (let i = 0; i < n; i++) {
+    const length = Math.floor((i / n) * max) + 1;
 
-  // for (let t = 0; t < tests; t++) {
-  //   let string = getRandomChars(Math.floor(((t + 1) / tests) * 2331));
-  //   localStorage.setItem("i" + t, string);
-  //   localStorage.setItem("o" + t, createQRCode(string));
-  // }
+    let string = getRandomChars(length);
+    let [url, symbol] = createQRCode(string);
 
-  for (let t = 0; t < tests; t++) {
-    const string = localStorage.getItem("i" + t);
-    try {
-      if (localStorage.getItem("o" + t) !== createQRCode(string)) {
-        throw new Error(`test ${t} didn't match`);
-      }
-    } catch (error) {
-      console.error(error);
+    let [imageData, dataURL] = await getImageData(url);
+
+    let result = jsQR(imageData.data, imageData.width, imageData.height);
+
+    img.src = url;
+
+    if (result == null) {
+      console.warn(`Missed ${i}`);
+      misses++;
+    } else if (result.data !== string) {
+      fails++;
+      console.error(`Failed ${i}!`, fails);
+    } else {
+      console.log(`%cPassed ${i}!`, "color: lightgreen;");
     }
   }
 
-  console.log("tests done");
-};
+  console.log("---------------------");
 
-img.src = localStorage.getItem("o" + 0);
-setTimeout(start, 50);
+  if (misses) {
+    console.warn("Total misses:", misses);
+  } else {
+    console.log("%cNo misses!", "color: lightgreen;");
+  }
+  if (fails) {
+    console.error("Total fails:", fails);
+  } else {
+    console.log("%cNo fails!", "color: lightgreen; font-weight: bold;");
+  }
+})();
