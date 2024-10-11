9a60e11da87ca53ccfbc8b5975390bf15fb72b77
Lasse H. Bomholt <lasse@bomh.net>
2024-10-11 20:50:13 +0200

    only use mask pattern 010

--- qrcode.js
+++ qrcode.js
@@ -145,184 +145,6 @@ BitMatrix.prototype.isReserved = function (row, col) {
   return this.reservedBit[row * this.size + col];
 };
 
-const MaskPattern = {};
-
-MaskPattern.Patterns = {
-  PATTERN000: 0,
-  PATTERN001: 1,
-  PATTERN010: 2,
-  PATTERN011: 3,
-  PATTERN100: 4,
-  PATTERN101: 5,
-  PATTERN110: 6,
-  PATTERN111: 7,
-};
-
-const PenaltyScores = {
-  N1: 3,
-  N2: 3,
-  N3: 40,
-  N4: 10,
-};
-
-MaskPattern.isValid = function isValid(mask) {
-  return mask != null && mask !== "" && !isNaN(mask) && mask >= 0 && mask <= 7;
-};
-
-MaskPattern.from = function from(value) {
-  return MaskPattern.isValid(value) ? parseInt(value, 10) : undefined;
-};
-
-MaskPattern.getPenaltyN1 = function getPenaltyN1(data) {
-  const size = data.size;
-  let points = 0;
-  let sameCountCol = 0;
-  let sameCountRow = 0;
-  let lastCol = null;
-  let lastRow = null;
-
-  for (let row = 0; row < size; row++) {
-    sameCountCol = sameCountRow = 0;
-    lastCol = lastRow = null;
-
-    for (let col = 0; col < size; col++) {
-      let module = data.get(row, col);
-      if (module === lastCol) {
-        sameCountCol++;
-      } else {
-        if (sameCountCol >= 5) points += PenaltyScores.N1 + (sameCountCol - 5);
-        lastCol = module;
-        sameCountCol = 1;
-      }
-
-      module = data.get(col, row);
-      if (module === lastRow) {
-        sameCountRow++;
-      } else {
-        if (sameCountRow >= 5) points += PenaltyScores.N1 + (sameCountRow - 5);
-        lastRow = module;
-        sameCountRow = 1;
-      }
-    }
-
-    if (sameCountCol >= 5) points += PenaltyScores.N1 + (sameCountCol - 5);
-    if (sameCountRow >= 5) points += PenaltyScores.N1 + (sameCountRow - 5);
-  }
-
-  return points;
-};
-
-MaskPattern.getPenaltyN2 = function getPenaltyN2(data) {
-  const size = data.size;
-  let points = 0;
-
-  for (let row = 0; row < size - 1; row++) {
-    for (let col = 0; col < size - 1; col++) {
-      const last =
-        data.get(row, col) +
-        data.get(row, col + 1) +
-        data.get(row + 1, col) +
-        data.get(row + 1, col + 1);
-
-      if (last === 4 || last === 0) points++;
-    }
-  }
-
-  return points * PenaltyScores.N2;
-};
-
-MaskPattern.getPenaltyN3 = function getPenaltyN3(data) {
-  const size = data.size;
-  let points = 0;
-  let bitsCol = 0;
-  let bitsRow = 0;
-
-  for (let row = 0; row < size; row++) {
-    bitsCol = bitsRow = 0;
-    for (let col = 0; col < size; col++) {
-      bitsCol = ((bitsCol << 1) & 0x7ff) | data.get(row, col);
-      if (col >= 10 && (bitsCol === 0x5d0 || bitsCol === 0x05d)) points++;
-
-      bitsRow = ((bitsRow << 1) & 0x7ff) | data.get(col, row);
-      if (col >= 10 && (bitsRow === 0x5d0 || bitsRow === 0x05d)) points++;
-    }
-  }
-
-  return points * PenaltyScores.N3;
-};
-
-MaskPattern.getPenaltyN4 = function getPenaltyN4(data) {
-  let darkCount = 0;
-  const modulesCount = data.data.length;
-
-  for (let i = 0; i < modulesCount; i++) darkCount += data.data[i];
-
-  const k = Math.abs(Math.ceil((darkCount * 100) / modulesCount / 5) - 10);
-
-  return k * PenaltyScores.N4;
-};
-
-function getMaskAt(maskPattern, i, j) {
-  switch (maskPattern) {
-    case MaskPattern.Patterns.PATTERN000:
-      return (i + j) % 2 === 0;
-    case MaskPattern.Patterns.PATTERN001:
-      return i % 2 === 0;
-    case MaskPattern.Patterns.PATTERN010:
-      return j % 3 === 0;
-    case MaskPattern.Patterns.PATTERN011:
-      return (i + j) % 3 === 0;
-    case MaskPattern.Patterns.PATTERN100:
-      return (Math.floor(i / 2) + Math.floor(j / 3)) % 2 === 0;
-    case MaskPattern.Patterns.PATTERN101:
-      return ((i * j) % 2) + ((i * j) % 3) === 0;
-    case MaskPattern.Patterns.PATTERN110:
-      return (((i * j) % 2) + ((i * j) % 3)) % 2 === 0;
-    case MaskPattern.Patterns.PATTERN111:
-      return (((i * j) % 3) + ((i + j) % 2)) % 2 === 0;
-
-    default:
-      throw new Error("bad maskPattern:" + maskPattern);
-  }
-}
-
-MaskPattern.applyMask = function applyMask(pattern, data) {
-  const size = data.size;
-
-  for (let col = 0; col < size; col++) {
-    for (let row = 0; row < size; row++) {
-      if (data.isReserved(row, col)) continue;
-      data.xor(row, col, getMaskAt(pattern, row, col));
-    }
-  }
-};
-
-MaskPattern.getBestMask = function getBestMask(data, setupFormatFunc) {
-  const numPatterns = Object.keys(MaskPattern.Patterns).length;
-  let bestPattern = 0;
-  let lowerPenalty = Infinity;
-
-  for (let p = 0; p < numPatterns; p++) {
-    setupFormatFunc(p);
-    MaskPattern.applyMask(p, data);
-
-    const penalty =
-      MaskPattern.getPenaltyN1(data) +
-      MaskPattern.getPenaltyN2(data) +
-      MaskPattern.getPenaltyN3(data) +
-      MaskPattern.getPenaltyN4(data);
-
-    MaskPattern.applyMask(p, data);
-
-    if (penalty < lowerPenalty) {
-      lowerPenalty = penalty;
-      bestPattern = p;
-    }
-  }
-
-  return bestPattern;
-};
-
 const ECCode = {};
 
 const EC_BLOCKS_TABLE = [
@@ -905,22 +727,20 @@ function createSymbol(data, version, errorCorrectionLevel, maskPattern) {
 
   setupData(modules, dataBits);
 
-  if (isNaN(maskPattern)) {
-    maskPattern = MaskPattern.getBestMask(
-      modules,
-      setupFormatInfo.bind(null, modules, errorCorrectionLevel)
-    );
+  for (let col = 0; col < size; col++) {
+    for (let row = 0; row < size; row++) {
+      if (modules.isReserved(row, col)) continue;
+      modules.xor(row, col, col % 3 === 0);
+    }
   }
 
-  MaskPattern.applyMask(maskPattern, modules);
-
-  setupFormatInfo(modules, errorCorrectionLevel, maskPattern);
+  setupFormatInfo(modules, errorCorrectionLevel, 2);
 
   return {
     modules: modules,
     version: version,
     errorCorrectionLevel: errorCorrectionLevel,
-    maskPattern: maskPattern,
+    maskPattern: 2,
     segments: segments,
   };
 }
